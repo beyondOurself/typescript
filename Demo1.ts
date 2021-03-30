2a7e2db2f9775966a5dba2c5983fc6e6c668e2d6
longlong <562172151@qq.com>
2021-03-30 16:38:07 +0800

    init

---
+++ Demo1.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-30 16:22:02
+ * @LastEditTime: 2021-03-30 16:25:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \v3\typescript\Demo1.ts
+ */
+
+function jslong() {
+    let web: string = 'hello world'
+    console.log(web)
+
+}
+jslong()
