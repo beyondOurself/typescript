a9aabef8277d7fb021c99b37f56451adb984ed1f
longlong <562172151@qq.com>
2021-04-23 16:37:03 +0800

    初识tsconfig

---
+++ Demo2.ts
@@ -0,0 +1,7 @@
+/*
+ * @Author: canlong.shen
+ * @Date: 2021-04-23 16:29:25
+ * @LastEditors: your name
+ * @LastEditTime: 2021-04-23 16:29:26
+ * @Description: file content
+ */
---
+++ 初识tsconfig.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: canlong.shen
+ * @Date: 2021-04-23 16:29:03
+ * @LastEditors: your name
+ * @LastEditTime: 2021-04-23 16:33:25
+ * @Description: file content
+ */
+
+// 输入 tsc init 新建 tsconfig.json 配置文件
+// 直接输入 tsc 就会从tsconfig.json  去编译ts文件
+const name2: string = "狗娃"
