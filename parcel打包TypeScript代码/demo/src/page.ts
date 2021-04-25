59c75fee92f6131fda02636960a3af5d7acd015f
longlong <562172151@qq.com>
2021-04-25 17:37:14 +0800

    parcel  打包文件

---
+++ parcel打包TypeScript代码/demo/src/page.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: canlong.shen
+ * @Date: 2021-04-25 17:21:08
+ * @LastEditors: your name
+ * @LastEditTime: 2021-04-25 17:21:21
+ * @Description: file content
+ */
+console.log("愚蠢的地球人")
